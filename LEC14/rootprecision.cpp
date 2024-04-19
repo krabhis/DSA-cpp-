1522a5989157ed3ed609619b935e3b6df95d4537
krabhis <abhishekkr212402@gmail.com>
2024-04-19 20:14:37 +0530

    binary search problems

---
+++ LEC14/rootprecision.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int rootInteger(int n){
+    int s=0,e=n;
+    int mid=s+((e-s)/2);
+    int ans=-1;
+    while(s<=e){
+        int square=mid*mid;
+        if (square==n){
+            return mid;
+        }
+        else if(square<n){  
+            ans=mid;
+            s=mid+1;
+
+        }
+        else{
+            e=mid-1;
+        }
+            mid=s+((e-s)/2);
+
+
+    }
+    return ans;
+}
+int main(){
+
+int a=rootInteger(36);
+cout<<"answer is"<<a;
+
+return 0;
+}
+
+
