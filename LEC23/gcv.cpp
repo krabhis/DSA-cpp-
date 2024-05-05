e869bf285000fac0fd6963e608378dc957f0375f
krabhis <abhishekkr212402@gmail.com>
2024-05-05 22:32:25 +0530

    2D array

---
+++ LEC23/gcv.cpp
@@ -0,0 +1,41 @@
+
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+
+    if(a==0)
+    return b;
+
+    if(b==0)
+    return a;
+
+    while(a != b) {
+
+        if(a>b)
+        {
+            a = a-b;
+        }
+        else{
+            b = b-a;
+        }
+    }
+    return b;  /*return a or b because while will iterate till a!=b as  a=b  iteration will stop*/
+}
+
+
+
+int main() {
+    
+    int a,b;
+    cout << "Enter the Values of a and b" << endl;
+    cin >> a >> b;
+
+    int ans = gcd(a,b);
+
+    cout << " The GCD of " << a << " & " << b << " is: " << ans << endl;
+
+
+
+    return 0;
+}
