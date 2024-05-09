58a61e15567f98c56c522394ea4e7ab3a5a105bc
krabhis <abhishekkr212402@gmail.com>
2024-05-09 16:51:51 +0530

    pattern problem

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,22 +1,42 @@
-// decimal  2 binary and binary 2 decimal is in note 
+#include <iostream>
+    using namespace std;
+    int main(){
+        int i,n,j,space,k,sum;
+        cout<<"enter the name"<<endl;
+        cin>>n;
+            i=1;
+        while (i<n){
+            j=1;
+            sum=1;
+            while(j<=n-i){
+                cout<<sum;
+                sum=sum+1;
+                j=j+1;
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n,ans,power;
-//     ans=0;
-//     power=1;
-//     cout<<"hello";
-//     cin>>n;
-//     while(n<0){
-//          int parityDigit=n%2;
-//          int temp=~(parityDigit);
-//          int pent=temp+1;
-//          ans+=pent*power;
-//          power*=10;
-//          n/=2;
-         
-//     }
-//     cout<<"answer is"<<ans;
-// }
 
+            }
+            k=i-1;
+            while(k){
+                cout<<"*";
+                k=k-1;
+            }
+            int h=i-1;
+            while(h){
+                cout<<"*";
+                h=h-1;
+
+            }
+            int g=1;
+            int kem=n-i;
+            while(g<n-i+1){
+                cout<<kem;
+                kem=kem-1;
+                g=g+1;
+            }
+            
+            cout<<endl;
+            i=i+1;
+
+
+        }
+    }
