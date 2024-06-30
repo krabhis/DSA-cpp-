4bec6fc1819e518cb6eec5893688b63e995ba079
krabhis <abhishekkr212402@gmail.com>
2024-06-30 21:35:13 +0530

    added code

---
+++ Practice/reverseLinkedlist.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+  public:
+    // Function to reverse a linked list.
+    struct Node* reverseList(struct Node* head) {
+        if(head==NULL || head->next==NULL)
+        return head;
+        
+        Node* prev=NULL;
+        Node*curr=head;
+        Node* forward=NULL;
+        
+        while(curr!=NULL){
+         forward=curr->next;
+         curr->next=prev;
+         prev=curr;
+         curr=forward;
+        
+        }
+        return prev;
+        
+    }
+};
