832077ee603d5bad3e59dab9863f96528c5de909
krabhis <abhishekkr212402@gmail.com>
2024-11-14 22:48:56 +0530

    added code

---
+++ LEC62_Binary tree/BT_traversal_linedlist.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    int data;
+    node*  left;
+    node* right;
+
+    node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+
+    }
+}
+
+void buildbt(node* root){
+    if(root!=NULL){
+        
+    }
+
+}
+
+int main(){
+
+}
