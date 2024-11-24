bee1b9e00b6930f25ba67a35ce7648e615425449
krabhis <abhishekkr212402@gmail.com>
2024-11-24 12:14:39 +0530

    added code

---
+++ 1_Practice_random_IMP/building_with_ocean_view_STACK.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main() {
+    vector<int>arr={4,3,2,1};
+    vector<int>result;
+    for(int i=0;i<arr.size();i++){
+        int maxi=*max_element(arr.begin()+i,arr.end());
+        if(maxi<=arr[i]){
+            result.push_back(i);
+        }
+
+    }
+     for(int i=0;i<result.size();i++){
+    cout<<result[i];
+ }
+    return 0;
+}
+
+
+
---
+++ 1_Practice_random_IMP/maximum_score_LC_1423.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int solve(vector<int>nums,int k){
+    int n=nums.size();
+    int r=0,l=0;
+    while(r<n){
+        
+    }
+
+
+
+}
+
+int main(){
+    vector<int>nums={1,2,3,4,5,6,1};
+    int k=3;
+    int result=solve(nums,k);
+    cout<<"maximum score is:->"<<result<<endl;
+
+}
